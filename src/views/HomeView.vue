2e8f1fbd9ad5db7528ef6e137598e48a88756808
Ibrahim Erfatpoor <ibrahim_feily@hotmail.com>
2025-02-20 12:03:33 +0100

    Second commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<!-- <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script> -->
 
 <template>
   <main>
-    <h1>Hejsan</h1>
+    <button>Logga in</button>
+    <button>Registrera dig</button>
   </main>
 </template>
