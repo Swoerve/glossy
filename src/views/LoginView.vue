050e221ccdafc00ba4a7570ee2c42f2c2dc106d1
Will Axelsson <will.axelsson@iths.se>
2025-02-20 15:18:46 +0100

    Fixa min förra commit

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from "vue"
 import { useRouter } from "vue-router"
+import { RouterLink, RouterView } from "vue-router";
 
 const router = useRouter();
 </script>
@@ -10,6 +11,7 @@ const router = useRouter();
     <main>
         <img src="../assets/glossy.png" alt="logo">
       <button href="">Logga in som elev</button>
+      <RouterLink to="./accountteacherview"></RouterLink>
       <button href="../views/AccountTeacherView.vue">Logga in som lärare</button>
 
     </main>
