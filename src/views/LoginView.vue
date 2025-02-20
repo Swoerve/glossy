4a034f62e0f722970390843b14da0f4b750b534b
Will Axelsson <will.axelsson@iths.se>
2025-02-20 14:29:01 +0100

    Fixade ett fel

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from "vue"
 import { useRoute } from "vue-router"
+import { RouterLink, RouterView } from "vue-router"
 
 const router = useRoute();
 </script>
@@ -11,7 +12,7 @@ const router = useRoute();
         <img src="glossy.png" alt="logo">
       <button>Logga in</button>
       <button>Registrera dig</button>
-      <router-link to "/">Tillbaka</router-link>
+      <router-link to="">Tillbaka</router-link>
 
     </main>
   </template>
