16c67829c8dabc9857f025cdb3b58f916ec7c32e
Max Asensio <max.asensio13@gmail.com>
2025-02-20 14:44:39 +0100

    SignUpView

---
+++ src/views/SignUpView.vue
@@ -0,0 +1,25 @@
+<script setup> 
+import { ref } from "vue"
+import { useRouter } from "vue-router"
+</script>
+
+<template>
+    <img src="Glossy\glossy\src\assets\glossy.png" alt="">
+    <div id="signup">
+        <p>Namn: {{ email }}</p>
+        <input v-model="email" placeholder="Skriv in din skolmail här!">
+        <select v-model="role">
+            <option disabled value="">Välj din roll</option>
+            <option>Lärare</option>
+            <option>Elev</option>
+        </select>
+        <p>Kurskod: {{ courseCode }}</p>
+        <input v-model="courseCode" placeholder="Skriv in din kurskod här!">
+        <input type="checkbox" id="terms" v-model="checked" />
+        <label for="terms"> {{ checked }}</label>
+    </div>
+</template>
+
+<style scoped>
+  
+</style>
