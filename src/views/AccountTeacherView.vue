c4035f7510c525878bdead2f4428b05194547cb4
Max Asensio <max.asensio13@gmail.com>
2025-02-21 12:38:34 +0100

    styling AccountTeacherView

--- src/views/AccountTeacherView.vue
+++ src/views/AccountTeacherView.vue
@@ -7,15 +7,50 @@
 </script>
 
 <template>
-  <h1>Välkommen</h1>
+  <div class="logo-container">
+    <img src="../assets/glossy.png" alt="logo" class="logo">
+  </div>
+
+  <div id="idk">
+  <h1>Välkommen lärare</h1>
   <div>
-    <button @click="showModal = true">Skapa en ny kurs</button>
+    <button @click="showModal = true" class="account-button">Skapa en ny kurs</button>
     <CreateCourse :isOpen="showModal" @close="showModal = false"></CreateCourse>
   </div>
-  <button>Statistik</button>
-  <button></button>
+  <button class="account-button">Statistik</button>
+  <button class="account-button">Inställningar</button>
+</div>
 </template>
 
 <style scoped>
-  
+
+.logo-container {
+  display: flex;
+  justify-content: center;
+  margin-left: 20px;
+}
+
+.logo {
+  width: 500px;
+  height: auto;
+}
+
+#idk {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+.account-button {
+  margin-top: 15px;
+  height: 40px;
+  width: 170px;
+  color: white;
+  background-color: #9667e0;
+  border: 0.5px solid #D4BBFC;
+  border-radius: 4px;
+}
+
 </style>
