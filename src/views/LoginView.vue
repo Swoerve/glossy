138039c61fa48e606544bbb303c7cc477e43ed0f
Max Asensio <max.asensio13@gmail.com>
2025-02-21 10:49:39 +0100

    styling LoginView

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -9,19 +9,53 @@ const router = useRouter();
 
 <template>
     <main>
-      <img src="../assets/glossy.png" alt="logo">
+      <div class="logo-container">
+        <img src="../assets/glossy.png" alt="logo" class="logo">
+      </div>
 
       <router-link  to="/accountstudentview">
-        <button>Logga in som elev</button>
+        <button class="login-button login-student-button" >Logga in som elev</button>
       </router-link>
 
       <router-link to="/accountteacherview">
-        <button>Logga in som lärare</button>
+        <button class="login-button login-teacher-button">Logga in som lärare</button>
       </router-link>
 
     </main>
   </template>
 
 <style scoped>
+.logo-container {
+  display: flex;
+  justify-content: center;
+}
 
+.logo {
+  width: 300px;
+  height: auto;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.login-button {
+  margin-top: 5px;
+  width: 170px;
+  border-radius: 4px;
+  color:white;
+  height: 40px;
+}
+
+.login-student-button {
+  background-color: #D4BBFC;
+  border: 0.5px solid #9667e0;
+}
+
+.login-teacher-button {
+  background-color: #9667e0;
+  border: 0.5px solid #d4bbfc
+}
 </style>
