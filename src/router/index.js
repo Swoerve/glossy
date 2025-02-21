e5066187ce0262977beb9d0e293da45b7b388f83
Ibrahim Erfatpoor <ibrahim_feily@hotmail.com>
2025-02-21 11:27:16 +0100

    changes

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import LoginView from '@/views/LoginView.vue'
 import AccountTeacherView from '@/views/AccountTeacherView.vue'
 import SignUpView from '@/views/SignUpView.vue'
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
