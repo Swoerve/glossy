f3e6e3d739a6cee38dfa1183d91f36de78d55f23
swoerve <swoerve@gmail.com>
2025-02-22 15:51:39 +0100

    created question component and made first iteration of quiz object

---
+++ src/components/quiz/Question.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+  const {question} = defineProps({
+    question: Object
+  })
+</script>
+
+<template>
+ <h1>{{ question.title }}</h1> 
+ <ul>
+    <li v-for="answer in question.answers">{{ answer }}</li>
+ </ul>
+</template>
+
+<style scoped>
+  
+</style>
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,9 +1,46 @@
 <script setup>
+import Question from "@/components/quiz/Question.vue"
+import { ref } from "vue";
+
+const quiz = ref({
+  title: "Quiz 1",
+  difficulty: 1,
+  questions: [{
+    title: "question 1",
+    answers: [
+      "answer 1",
+      "answer 2",
+      "answer 3",
+      "answer 4"
+    ],
+    correctAnswer: 1
+  }, {
+    title: "question 2",
+    answers: [
+      "answer 1",
+      "answer 2",
+      "answer 3",
+      "answer 4"
+    ],
+    correctAnswer: 0
+  }],
+  stars: 3
+})
+
+const currQuestion = ref(0)
 
 </script>
 
-<template>
+<!-- 
+have a quiz object that contains all necessary info?
+then retrieve that info and put it into question components?
+-->
 
+<template>
+<h1>{{quiz.title}}</h1>
+<Question :question="quiz.questions[currQuestion]"></Question>
+<button @click="currQuestion-=1">Prev Question</button>
+<button @click="currQuestion+=1">Next Question</button>
 </template>
 
 <style>
