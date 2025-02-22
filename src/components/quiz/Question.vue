9d15c8ad717b83ea1f1448c78ba28940dd5f55b9
swoerve <swoerve@gmail.com>
2025-02-22 17:34:57 +0100

    working quiz and question system/vie

--- src/components/quiz/Question.vue
+++ src/components/quiz/Question.vue
@@ -1,17 +1,31 @@
 <script setup>
 
-  const {question} = defineProps({
-    question: Object
-  })
+// props
+const {question, qid} = defineProps({
+  question: Object,
+  qid: Number
+})
+
+// emits
+defineEmits(["answered"])
+
 </script>
 
 <template>
  <h1>{{ question.title }}</h1> 
- <ul>
-    <li v-for="answer in question.answers">{{ answer }}</li>
- </ul>
+ <div class="answers">
+    <button v-for="answer, ind in question.answers" :value="ind" @click="$emit('answered', [ind, qid])">{{ answer }}</button>
+ </div>
 </template>
 
 <style scoped>
+  
+  .answers {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    max-width: 50%;
+    max-height: 50%;
+  }
   
 </style>
