b84dfcfe54eb8ca334a44282c3edbe95f8953028
swoerve <swoerve@gmail.com>
2025-02-27 13:57:26 +0100

    fixed a routing problem for students

--- src/router/index.js
+++ src/router/index.js
@@ -1,66 +1,66 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LoginView from '@/views/LoginView.vue'
-import AccountTeacherView from '@/views/AccountTeacherView.vue'
-import SignUpView from '@/views/SignUpView.vue'
-import AccountStudentView from '@/views/AccountStudentView.vue'
-import CreateQuizView from '@/views/CreateQuizView.vue'
-import QuizView from '@/views/QuizView.vue'
-import CreateCourseView from '@/views/CreateCourseView.vue'
-import TeacherCourseView from '@/views/TeacherCourseView.vue'
+import { createRouter, createWebHistory } from "vue-router"
+import HomeView from "../views/HomeView.vue"
+import LoginView from "@/views/LoginView.vue"
+import AccountTeacherView from "@/views/AccountTeacherView.vue"
+import SignUpView from "@/views/SignUpView.vue"
+import AccountStudentView from "@/views/AccountStudentView.vue"
+import CreateQuizView from "@/views/CreateQuizView.vue"
+import QuizView from "@/views/QuizView.vue"
+import CreateCourseView from "@/views/CreateCourseView.vue"
+import TeacherCourseView from "@/views/TeacherCourseView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView
     },
     {
-      path: '/login',
-      name: 'login',
+      path: "/login",
+      name: "login",
 
-      component: () => import('../views/LoginView.vue')
+      component: () => import("../views/LoginView.vue")
     },
     {
-      path: '/loginview',
-      name: 'loginview',
+      path: "/loginview",
+      name: "loginview",
       component: LoginView
     },
     {
-      path: '/teacher/:userid',
-      name: 'accountteacherview',
+      path: "/teacher/:userid",
+      name: "accountteacherview",
       component: AccountTeacherView
     },
     {
-      path: '/signupview',
-      name: 'signupview',
+      path: "/signupview",
+      name: "signupview",
       component: SignUpView
     },
     {
-      path: '/accountstudentview',
-      name: 'studentview',
+      path: "/student/:studentid",
+      name: "studentview",
       component: AccountStudentView
     },
     {
-      path: '/teacher/:userid/course/:courseid',
-      name: 'TeacherCourseView',
+      path: "/teacher/:userid/course/:courseid",
+      name: "TeacherCourseView",
       component: TeacherCourseView
     },
     {
-      path: '/teacher/:userid/course/:courseid/newquiz',
-      name: 'newquiz',
+      path: "/teacher/:userid/course/:courseid/newquiz",
+      name: "newquiz",
       component: CreateQuizView
     },
     {
-      path: '/accountstudentview/quizview',
-      name: 'quizview',
+      path: "/accountstudentview/quizview",
+      name: "quizview",
       component: QuizView
     },
     {
-      path: '/createcourseview',
-      name: 'createcourseview',
+      path: "/createcourseview",
+      name: "createcourseview",
       component: CreateCourseView
     }
   ]
