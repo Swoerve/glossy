47225d6e227876df33c0f49c207960b69c2fe06c
swoerve <swoerve@gmail.com>
2025-03-04 13:04:30 +0100

    created g-card

---
+++ src/components/g-card.vue
@@ -0,0 +1,37 @@
+<script setup>
+  import { ref } from "vue"
+  const { title } = defineProps({
+    title: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: Boolean
+    },
+    routeTo: {
+      type: String
+    }
+  })
+</script>
+
+<template>
+  <template v-if="link">
+    <div>
+      <router-link>
+        <div>
+          <h1>{{ title }}</h1>
+        </div>
+      </router-link>
+    </div>
+  </template>
+  <template v-else>
+    <div>
+      <h1>{{ title }}</h1>
+    </div>
+  </template>
+</template>
+
+<style scoped>
+  div {
+  }
+</style>
