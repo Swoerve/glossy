e339d3e44738d1769d8354cf1b9447327cf79475
swoerve <swoerve@gmail.com>
2025-03-07 16:25:56 +0100

    fixed student change thingy

--- src/views/student/StudentProfileView.vue
+++ src/views/student/StudentProfileView.vue
@@ -1,53 +1,58 @@
 <script setup>
-import { ref } from "vue";
-import { getSessionStorage } from "@/storageHandler";
-import { RouterLink } from "vue-router";
-import Button from "@/components/buttonComponent.vue";
-import userSettings from "@/components/user-settings.vue";
+  import { ref } from "vue"
+  import { getSessionStorage, replaceLocalStorage } from "@/storageHandler"
+  import { RouterLink } from "vue-router"
+  import Button from "@/components/buttonComponent.vue"
+  import userSettings from "@/components/user-settings.vue"
 
-const student = ref({
-  name: "",
-  email: "",
-  password: ""
-});
-const showSettings = ref(false);
-const showEmailModal = ref(false);
-const showPasswordModal = ref(false);
-const newEmail = ref("");
-const newPassword = ref("");
+  const student = ref({
+    name: "",
+    email: "",
+    password: ""
+  })
+  const showSettings = ref(false)
+  const showEmailModal = ref(false)
+  const showPasswordModal = ref(false)
+  const newEmail = ref("")
+  const newPassword = ref("")
 
-if (getSessionStorage("loggedin")) {
-  student.value = getSessionStorage("loggedin");
-}
-
-// Function to update email
-const updateEmail = () => {
-  if (newEmail.value) {
-    student.value.email = newEmail.value;
-    newEmail.value = ""; 
-    showEmailModal.value = false; 
+  if (getSessionStorage("loggedin")) {
+    student.value = getSessionStorage("loggedin")
   }
-};
 
-// Function to update password
-const updatePassword = () => {
-  if (newPassword.value) {
-    student.value.password = newPassword.value;
-    newPassword.value = ""; 
-    showPasswordModal.value = false; 
+  // Function to update email
+  const updateEmail = () => {
+    if (newEmail.value) {
+      student.value.email = newEmail.value
+      newEmail.value = ""
+      showEmailModal.value = false
+      replaceLocalStorage("students", student.value)
+    }
   }
-};
+
+  // Function to update password
+  const updatePassword = () => {
+    if (newPassword.value) {
+      student.value.password = newPassword.value
+      newPassword.value = ""
+      showPasswordModal.value = false
+      replaceLocalStorage("students", student.value)
+    }
+  }
 </script>
 
 <template>
   <h1>{{ student.name }}s profil</h1>
   <div id="picture-container">
-    <img src="http://imgc.allpostersimages.com/images/P-473-488-90/68/6896/2GOJ100Z/posters/despicable-me-2-minions-movie-poster.jpg" class="image-cover" />
+    <img
+      src="http://imgc.allpostersimages.com/images/P-473-488-90/68/6896/2GOJ100Z/posters/despicable-me-2-minions-movie-poster.jpg"
+      class="image-cover"
+    />
   </div>
   <label for="img">Select image:</label>
   <input type="file" id="img" name="img" accept="image/*" />
   <input type="submit" />
-  
+
   <!-- Display email -->
   <p>E-mail: {{ student.email }}</p>
   <Button
@@ -65,7 +70,7 @@ const updatePassword = () => {
   />
 
   <br />
-  
+
   <!-- Settings Button -->
   <div>
     <Button
@@ -91,7 +96,11 @@ const updatePassword = () => {
   <div v-if="showPasswordModal" class="modal">
     <div class="modal-content">
       <h2>Ändra Lösenord</h2>
-      <input type="password" v-model="newPassword" placeholder="Nytt lösenord" />
+      <input
+        type="password"
+        v-model="newPassword"
+        placeholder="Nytt lösenord"
+      />
       <button @click="updatePassword">Spara</button>
       <button @click="showPasswordModal = false">Avbryt</button>
     </div>
@@ -99,32 +108,32 @@ const updatePassword = () => {
 </template>
 
 <style scoped>
-h1 {
-  font-size: 3em;
-}
+  h1 {
+    font-size: 3em;
+  }
 
-.picture-container {
-  width: 150px;
-}
+  .picture-container {
+    width: 150px;
+  }
 
-.image--cover {
-  width: 150px;
-  height: 180px;
-}
+  .image--cover {
+    width: 150px;
+    height: 180px;
+  }
 
-.modal {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0,0,0,0.5);
-}
+  .modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
 
-.modal-content {
-  background-color: white;
-  padding: 20px;
-  margin: auto;
-  margin-top: calc(50vh - 100px);
-}
+  .modal-content {
+    background-color: white;
+    padding: 20px;
+    margin: auto;
+    margin-top: calc(50vh - 100px);
+  }
 </style>
