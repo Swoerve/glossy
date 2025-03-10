66d2fb0a9e643dece9fd5287d8ff1994fe0d7e62
Ibrahim Erfatpoor <ibrahim_feily@hotmail.com>
2025-03-10 14:00:36 +0100

    fixade rätt och fel

--- src/views/statistics/CourseStatisticsView.vue
+++ src/views/statistics/CourseStatisticsView.vue
@@ -1,73 +1,72 @@
+
 <script setup>
-  import { ref, computed } from "vue"
-  import { getLocalStorage, getSessionStorage } from "@/storageHandler"
-  import { useRoute } from "vue-router"
+import { ref, computed } from "vue"
+import { getLocalStorage, getSessionStorage } from "@/storageHandler"
+import { useRoute } from "vue-router"
 
-  const route = useRoute()
-  const students = ref([])
-  const quizzes = ref([])
+const route = useRoute()
+const students = ref([])
+const quizzes = ref([])
 
-  const statistics = getLocalStorage("statistics").filter((obj) => {
-    return obj.courseid === route.params.courseid
-  })
+const statistics = getLocalStorage("statistics").filter((obj) => {
+  return obj.courseid === route.params.courseid
+})
 
-  const course = getLocalStorage("courses").filter((obj) => {
-    console.log(obj.id)
-    return obj.id === route.params.courseid
-  })[0]
-  console.log(course)
+const course = getLocalStorage("courses").filter((obj) => {
+  return obj.id === route.params.courseid
+})[0]
 
-  //En forEach som returnerar eleverna från kursen
-  course.students.forEach((student) => {
-    students.value.push(
-      getLocalStorage("students").filter((obj) => {
-        console.log("stat")
-        console.log(student)
-        console.log(obj.id)
-        console.log(obj.id === student)
-        if (obj.id === student) {
-          return true
+// Hämtar elever från kursen
+course.students.forEach((student) => {
+  const foundStudent = getLocalStorage("students").find((obj) => obj.id === student)
+  if (foundStudent) students.value.push(foundStudent)
+})
+
+// Hämtar quizzes
+course.quizzes.forEach((quiz) => {
+  quizzes.value.push(quiz)
+})
+
+// Funktion för att beräkna rätt och fel svar för varje quiz
+const quizResults = computed(() => {
+  return quizzes.value.map((quiz) => {
+    const quizStats = statistics.filter((stat) => stat.quizid === quiz.id)
+
+    let correctAnswers = 0
+    let incorrectAnswers = 0
+
+    quizStats.forEach((stat) => {
+      stat.answers.forEach((answer) => {
+        if (answer.correct) {
+          correctAnswers++
+        } else {
+          incorrectAnswers++
         }
-      })[0]
-    )
+      })
+    })
+
+    return {
+      id: quiz.id,
+      title: quiz.title,
+      correctAnswers,
+      incorrectAnswers
+    }
   })
-  console.log(students.value)
-  console.log(statistics)
+})
 
-  console.log(statistics[0].studentid)
-
-  course.quizzes.forEach((quiz) => {
-    quizzes.value.push(
-      course.quizzes.filter((obj) => {
-        console.log(quiz)
-        return obj.id === quiz.id
-      })[0]
-    )
-  })
-
-  // student.value = statistics[0].student
-
-  //En funktion för att filtrera och hitta kursens namn
-  function getCourseName() {
-    const courses = getLocalStorage("courses")
-    const currentCourse = courses.find(
-      (course) => course.id === route.params.courseid
-    )
-    return currentCourse ? currentCourse.name : "Kursen Hittades Inte"
-  }
-
-  // const data = getLocalStorage("statistics").filter((obj) => {
-  //   return obj.courseid === route.params.courseid
-  // })
-
-  // console.log(data)
-  // console.log(data[0].try)
-  // console.log(data[0].answers)
-  // console.log(data[1].try)
-  // console.log(data[1].answers)
+// Hämta kursnamn
+function getCourseName() {
+  const courses = getLocalStorage("courses")
+  const currentCourse = courses.find(
+    (course) => course.id === route.params.courseid
+  )
+  return currentCourse ? currentCourse.name : "Kursen Hittades Inte"
+}
 </script>
+
 <template>
   <h1>Statistik för {{ getCourseName() }}</h1>
+
   <h2>Elever</h2>
   <ul>
     <li v-for="(student, index) in students" :key="index">
@@ -78,13 +77,19 @@
       </router-link>
     </li>
   </ul>
+
   <h3>Quiz</h3>
   <ul>
-    <li v-for="(quiz, index) in quizzes" :key="index">
+    <li v-for="(quiz, index) in quizResults" :key="index">
       <router-link
         :to="`/teacher/${route.params.userid}/course/${course.id}/quiz/${quiz.id}/quizstatistics`"
-        >{{ quiz.title }}</router-link
       >
+        {{ quiz.title }}
+      </router-link>
+      <!-- Visa antal rätt och fel -->
+      <p>
+         Rätt: {{ quiz.correctAnswers }} |  Fel: {{ quiz.incorrectAnswers }}
+      </p>
     </li>
   </ul>
 </template>
